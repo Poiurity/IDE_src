f2d40ca38f601adfd0ea8a4a37d55eada8dd0bef
Poiurity <69443172+Poiurity@users.noreply.github.com>
2021-09-15 14:44:24 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,22 @@
+#define PIN7 7
+
+void setup(){
+  pinMode(PIN7, OUTPUT);
+}
+
+void loop(){
+  digitalWrite(PIN7, LOW);
+  delay(1000);
+
+  int i = 0;
+  for(;i<5;i++){
+    digitalWrite(PIN7, HIGH);
+    delay(100);
+    digitalWrite(PIN7, LOW);
+    delay(100);
+  }
+  digitalWrite(PIN7, HIGH);
+  while(1){
+  
+  }
+}
